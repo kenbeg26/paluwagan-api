02d09c7005a15c729f9ddec4724ce9aabd37d748
kenbegornia2 <kenbegornia2@gmail.com>
2025-08-11 22:15:42 +0800

    not finished route and controller add product

--- controllers/product.js
+++ controllers/product.js
@@ -2,3 +2,23 @@ const bcrypt = require("bcrypt");
 const Product = require("../models/Product");
 const auth = require("../auth");
 const { errorHandler } = auth;
+
+// Create Product
+module.exports.addProduct = (req, res) => {
+  const { name, amount, number } = req.body;
+
+  if (!name || !amount || !number) {
+    return res.status(400).json({ error: "All fields (name, amount, number) are required" });
+  }
+
+  const newProduct = new Product({
+    name,
+    amount,
+    number
+  });
+
+  newProduct.save()
+    .then(product => {
+      res.status(201)
+    })
+}
--- routes/product.js
+++ routes/product.js
@@ -6,5 +6,15 @@ const { verify, verifyAdmin } = require("../auth");
 // Routing Component
 const router = express.Router();
 
+router.post("/", verify, verifyAdmin, productController.addProduct);
+
+// router.get("/all", verify, verifyAdmin, productController.getAllProducts);
+
+//router.get("/active", productController.getActiveProducts);
+
+//router.get("/:productId", productController.getProductById);
+
+
+
 
 module.exports = router;
